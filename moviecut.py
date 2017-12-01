a26be45ab2e3f99b6f6b0a2d9dae69ea82226861
emiludenz <emilbh@gmail.com>
2017-12-01 18:33:03 +0100

    cleaned up a bit

--- moviecut.py
+++ moviecut.py
@@ -1,8 +1,28 @@
 from moviepy.editor import *
-import os, shutil
+import os
+import shutil
 import csv
 
 
+def main():
+    # Home dir - change it to something not static
+    home = os.getcwd()
+    # Clips folder
+    folder = home + "\\clips"
+
+    # Make dir if it does not exist
+    try:
+        os.mkdir(folder)
+    except FileExistsError:
+        pass
+    video_clip = get_video_clip()
+
+    marks = read_marks()
+    for key, val in marks.items():
+        start, stop = val[0], val[1]
+        clip_name = key[2:-1] + ".mp4"
+        cut_out(video_clip, clip_name, start, stop)
+        shutil.move(home + "\\" + clip_name, folder + "\\" + clip_name)
 
 
 def read_marks():
@@ -26,34 +46,13 @@ def cut_out(video_clip, clip_name, start, stop):
     clip = VideoFileClip(video_clip).subclip(start, stop)
     clip.write_videofile(clip_name)
 
+
 def get_video_clip():
     content = os.listdir()
     for item in content:
         if ".mp4" in item:
             return item
 
-def main():
-    # Home dir - change it to something not static
-    home = os.getcwd()
-    # Clips folder
-    folder = home + "\\clips"
-
-    # Make dir if it does not exist
-    try:
-        os.mkdir(folder)
-    except FileExistsError:
-        pass
-    video_clip = get_video_clip()
-
-    # Hardcoded checker
-    # video_clip = "2017.11.16 - Godnatlæsning med Paludan Afsnit 14.mp4"
-
-    marks = read_marks()
-    for key, val in marks.items():
-        start, stop = val[0], val[1]
-        clip_name = key[2:-1] + ".mp4"
-        cut_out(video_clip, clip_name, start, stop)
-        shutil.move(home + "\\" + clip_name, folder + "\\" + clip_name)
 
 if __name__ == "__main__":
     main()
